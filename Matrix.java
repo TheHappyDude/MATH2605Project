df970013a32cfc083b49cd10c9590bc0c719c450
othornycroft <othornycroft@gmail.com>
2015-03-16 07:49:27 -0400

    Set up headers for basic program layout

--- Matrix.java
+++ Matrix.java
@@ -5,4 +5,31 @@ public class Matrix() {
 
     private double[][] matrix;
 
+    public Matrix(double[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public Matrix[] LUFactorize() {
+
+    }
+
+    public Matrix[] QRFactorize() {
+
+    }
+
+    public double getDeterminant() {
+
+    }
+
+    public double[] getEigenvalues() {
+
+    }
+
+    public double[] getEigenvectors() {
+
+    }
+
+    public Matrix getTranspose() {
+
+    }
 }
