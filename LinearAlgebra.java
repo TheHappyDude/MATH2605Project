cea3eb35a13393c4e2d17d4c0fcf962549df92df
othornycroft <othornycroft@gmail.com>
2015-03-16 07:45:20 -0400

    Set up headers for basic program layout

---
+++ LinearAlgebra.java
@@ -0,0 +1,33 @@
+public class LinearAlgebra {
+    public LinearAlgebra() {
+
+    }
+
+    public Vector dot(Vector v1,Vector v2) {
+
+    }
+    
+    public Vector scale(double c1, Vector v1) {
+
+    }
+
+    public Vector add(Vector v1,Vector v2) {
+
+    }
+
+    public Vector multMatrixVector(Matrix m1,Vector v1) {
+
+    }
+
+    public Matrix multMatrices(Matrix m1,Matrix m2) {
+
+    }
+
+    public Matrix scale(double c1, Matrix m1) {
+
+    }
+
+    public Matrix add(Matrix m1,Matrix m2) {
+
+    }
+}
---
+++ Vector.java
@@ -0,0 +1,11 @@
+public class Vector {
+    private double[] vector;
+
+    public Vector(double[] vector) {
+        this.vector = vector;
+    }
+
+    public Vector getTranspose() {
+
+    }
+}
